75d13857c06f834329419d0b104ca6d1db9055c0
vittolym <lexymdz@gmail.com>
2026-02-11 22:12:39 -0300

    init swagger info

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 const cookieParser = require('cookie-parser');
 import 'dotenv/config';
 
+const config = new DocumentBuilder()
+  .setTitle('Ecommerce API')
+  .setDescription('Professional ecommerce backend with payments and observability')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
